33cf27e53c30b2f5f2caae8cfc3b340e8f73dc58
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-03-06 12:49:09 -0500

    add opts for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,31 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-    },
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            panel = {
+                enabled = true,
+                auto_refresh = true,
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                keymap = {
+                    accept = "<Tab>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>",
+                },
+            },
+            filetypes = {
+                -- Enable for all filetypes
+                ["*"] = true,
+            },
+            copilot_node_command = 'node', -- Node.js version to use
+            server_opts_overrides = {},
+        })
+    end,
 }
